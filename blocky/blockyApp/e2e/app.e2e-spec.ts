1d02819cdbbc9d18cf91b230a008bd88b0136ffc
mista-j <johnnie.a.hernandez@gmail.com>
2018-10-07 09:44:44 -0400

    rest works, api works, containers work, cluster management work, google cloud is not working

---
+++ blocky/blockyApp/e2e/app.e2e-spec.ts
@@ -0,0 +1,161 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { AngularTestPage } from './app.po';
+import { ExpectedConditions, browser, element, by } from 'protractor';
+import {} from 'jasmine';
+
+
+describe('Starting tests for blockyApp', function() {
+  let page: AngularTestPage;
+
+  beforeEach(() => {
+    page = new AngularTestPage();
+  });
+
+  it('website title should be blockyApp', () => {
+    page.navigateTo('/');
+    return browser.getTitle().then((result)=>{
+      expect(result).toBe('blockyApp');
+    })
+  });
+
+  it('network-name should be blocky@1.0.0',() => {
+    element(by.css('.network-name')).getWebElement()
+    .then((webElement) => {
+      return webElement.getText();
+    })
+    .then((txt) => {
+      expect(txt).toBe('blocky@1.0.0.bna');
+    });
+  });
+
+  it('navbar-brand should be blockyApp',() => {
+    element(by.css('.navbar-brand')).getWebElement()
+    .then((webElement) => {
+      return webElement.getText();
+    })
+    .then((txt) => {
+      expect(txt).toBe('blockyApp');
+    });
+  });
+
+  
+    it('ConsumerInfo component should be loadable',() => {
+      page.navigateTo('/ConsumerInfo');
+      browser.findElement(by.id('assetName'))
+      .then((assetName) => {
+        return assetName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('ConsumerInfo');
+      });
+    });
+
+    it('ConsumerInfo table should have 4 columns',() => {
+      page.navigateTo('/ConsumerInfo');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(4); // Addition of 1 for 'Action' column
+      });
+    });
+  
+
+  
+    it('Consumer component should be loadable',() => {
+      page.navigateTo('/Consumer');
+      browser.findElement(by.id('participantName'))
+      .then((participantName) => {
+        return participantName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('Consumer');
+      });
+    });
+
+    it('Consumer table should have 9 columns',() => {
+      page.navigateTo('/Consumer');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(9); // Addition of 1 for 'Action' column
+      });
+    });
+  
+    it('Client component should be loadable',() => {
+      page.navigateTo('/Client');
+      browser.findElement(by.id('participantName'))
+      .then((participantName) => {
+        return participantName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('Client');
+      });
+    });
+
+    it('Client table should have 4 columns',() => {
+      page.navigateTo('/Client');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(4); // Addition of 1 for 'Action' column
+      });
+    });
+  
+    it('Service component should be loadable',() => {
+      page.navigateTo('/Service');
+      browser.findElement(by.id('participantName'))
+      .then((participantName) => {
+        return participantName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('Service');
+      });
+    });
+
+    it('Service table should have 4 columns',() => {
+      page.navigateTo('/Service');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(4); // Addition of 1 for 'Action' column
+      });
+    });
+  
+    it('Regulator component should be loadable',() => {
+      page.navigateTo('/Regulator');
+      browser.findElement(by.id('participantName'))
+      .then((participantName) => {
+        return participantName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('Regulator');
+      });
+    });
+
+    it('Regulator table should have 4 columns',() => {
+      page.navigateTo('/Regulator');
+      element.all(by.css('.thead-cols th')).then(function(arr) {
+        expect(arr.length).toEqual(4); // Addition of 1 for 'Action' column
+      });
+    });
+  
+
+  
+    it('SampleTransaction component should be loadable',() => {
+      page.navigateTo('/SampleTransaction');
+      browser.findElement(by.id('transactionName'))
+      .then((transactionName) => {
+        return transactionName.getText();
+      })
+      .then((txt) => {
+        expect(txt).toBe('SampleTransaction');
+      });
+    });
+  
+
+});
